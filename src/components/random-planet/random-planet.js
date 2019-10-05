7715b50aed1536f6d09a9b6f64a41dd4d0729bfa
alexey <aleshakoL@gmail.com>
2019-10-05 17:09:16 +0300

    list upd

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -15,9 +15,8 @@ export default class RandomPlanet extends Component {
       error: false
     }
 
-    constructor() {
-        super();
-        this.updatePlanet();
+    componentDidMount() {
+      this.updatePlanet();
     }
 
     onPlanetLoaded = (planet) => {
@@ -36,7 +35,7 @@ export default class RandomPlanet extends Component {
     }
 
     updatePlanet() {
-        const id = Math.floor(Math.random()*20) + 2;
+        const id = Math.floor(Math.random()*17) + 2;
         this.swapiService.getPlanet(id)
              .then(this.onPlanetLoaded)
              .catch(this.onError)
